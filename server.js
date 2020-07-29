6cd1e0ed9a77e028611fe9d183eab00a9bf81bca
Alba Osmanaj <albaos91@gmail.com>
2020-07-28 22:20:57 -0500

    added app.listen

--- server.js
+++ server.js
@@ -20,3 +20,7 @@ const routes = require("./controllers/controller");
 app.use("/", routes);
 
 app.listen(port);
+
+app.listen(PORT, function () {
+  console.log("Server listening on http://localhost:" + PORT);
+});
